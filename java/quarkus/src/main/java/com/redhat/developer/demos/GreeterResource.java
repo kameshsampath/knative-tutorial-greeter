5ff7bddf53b4d71d104a9fd1093e2ca008088c5c
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-04-02 18:49:01 +0530

    (fix): Make Event Greeter superset of greeter
    - Adding post method to greeter to handle the event greeter logic
    - Added Quarkus logic for handling event greeting
    
    Fixes https://github.com/redhat-developer-demos/knative-tutorial/issues/67

--- java/quarkus/src/main/java/com/redhat/developer/demos/GreeterResource.java
+++ java/quarkus/src/main/java/com/redhat/developer/demos/GreeterResource.java
@@ -3,9 +3,7 @@ package com.redhat.developer.demos;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/")
@@ -23,6 +21,14 @@ public class GreeterResource {
   @Path("/")
   public String greet() {
     return greetingService.greet(messagePrefix);
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("/")
+  public String eventGreet(String cloudEventJson) {
+    return greetingService.eventGreet(cloudEventJson);
   }
 
   @GET
--- java/springboot/src/main/java/com/example/demo/GreeterController.java
+++ java/springboot/src/main/java/com/example/demo/GreeterController.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * GreeterController
@@ -10,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreeterController {
 
-    private static final String RESPONSE_STRING_FORMAT = "%s greeter => '%s' : %d\n";
+    private static final Logger LOGGER = LoggerFactory.getLogger(GreeterController.class);
+
+    private static final String RESPONSE_STRING_FORMAT = "%s %s greeter => '%s' : %d\n";
+
+    private final SimpleDateFormat SDF = new SimpleDateFormat("HH:mm:ss");
 
     private static final String HOSTNAME =
-            parseContainerIdFromHostname(System.getenv().getOrDefault("HOSTNAME", "unknown"));
+        parseContainerIdFromHostname(System.getenv().getOrDefault("HOSTNAME", "unknown"));
 
     static String parseContainerIdFromHostname(String hostname) {
         return hostname.replaceAll("greeter-v\\d+-", "");
@@ -27,10 +36,23 @@ public class GreeterController {
      */
     private int count = 0;
 
+
     @GetMapping("/")
     public String greet() {
         count++;
-        return String.format(RESPONSE_STRING_FORMAT, prefix, HOSTNAME, count);
+        return String.format(RESPONSE_STRING_FORMAT, prefix, "", HOSTNAME, count);
+    }
+
+    @PostMapping("/")
+    public @ResponseBody
+    String eventGreet(@RequestBody String cloudEventJson) {
+        count++;
+        String greeterHost = String.format(RESPONSE_STRING_FORMAT, ""," Event ", HOSTNAME, count);
+        JsonObject response = new JsonObject(cloudEventJson)
+                                  .put("host",greeterHost.replace("\n","").trim())
+                                  .put("time",SDF.format(new Date()));
+        LOGGER.info("Event Message Received \n {}",response.encodePrettily());
+        return response.encode();
     }
 
     @GetMapping("/healthz")
