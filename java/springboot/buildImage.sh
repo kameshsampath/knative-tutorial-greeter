0e12486e8cec07f0cdd38397855342f411d414aa
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-04-04 19:59:46 +0530

    fix the Dockerfile.all FROM image

--- java/springboot/buildImage.sh
+++ java/springboot/buildImage.sh
@@ -5,4 +5,4 @@
 _NAME=$(xml sel -N ns="http://maven.apache.org/POM/4.0.0" -t -v "//ns:project/ns:artifactId/text()" ./pom.xml)
 _VERSION=$(xml sel -N ns="http://maven.apache.org/POM/4.0.0" -t -v "//ns:project/ns:version/text()" ./pom.xml)
 
-docker build -t ${DOCKER_USER:-dev.local/rhdevelopers}/${_NAME}:${_VERSION} .
+docker build -t ${DOCKER_USER:-dev.local/rhdevelopers}/${_NAME}:${_VERSION} -f Dockerfile.all .
